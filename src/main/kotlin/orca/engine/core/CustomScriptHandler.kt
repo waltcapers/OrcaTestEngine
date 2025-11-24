f6cdff6a6d4bbffd1ae2e8edee4e9d255bd9dd9e
waltercapers <wcapers64@gmail.com>
2025-11-24 09:50:32 -0500

    Added licensing to source files.

--- src/main/kotlin/orca/engine/core/CustomScriptHandler.kt
+++ src/main/kotlin/orca/engine/core/CustomScriptHandler.kt
@@ -1,3 +1,22 @@
+/*
+ * Dual License: MIT + GM Exception
+ * Copyright (c) 2025 Walter E. Capers
+ *
+ * MIT License applies to all users except General Motors (GM).
+ *
+ * GM Exception:
+ * GM is granted a perpetual, worldwide, royalty-free license to use, modify,
+ * reproduce, distribute, and create derivative works from this Software for any
+ * business or commercial purpose. This exception applies only to GM and does
+ * not extend to other third parties.
+ *
+ * MIT License:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files to deal in the Software
+ * without restriction.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ */
 package orca.engine.core
 import orca.engine.model.ScriptDefinition
 import java.io.File
